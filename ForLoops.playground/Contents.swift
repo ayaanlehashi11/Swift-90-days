6c900441901a0e2ee6ae543bae55ef3a55837496
Ayaanle hashi <91608871+ayaanlehashi11@users.noreply.github.com>
2023-02-23 13:41:08 +0300

    For-loops
    For-loops

---
+++ ForLoops.playground/Contents.swift
@@ -0,0 +1,24 @@
+// For-loops introduction
+
+// a simple range based for-loop
+for index in 1..5{
+  print("\(index) times 5 is \(index * 5)")
+}
+// 1 times 5 is 5
+// 2 times 5 is 10
+// 3 times 5 is 15
+// 4 times 5 is 20
+// 5 times 5 is 25
+
+// array iteration using for-loop
+
+let names = ["ayaanle" , "axmed" , "cismaan" , "cabdiraxmaan"]
+for name in names
+{
+   print("Name: \(name)")
+}
+//output:
+//Name: ayaanle
+//Name: axmed
+//Name: cismaan
+//Name: cabdiqani
