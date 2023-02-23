175dab4e413ec8276d9a3c9f7a853cf7f75230ce
Ayaanle hashi <91608871+ayaanlehashi11@users.noreply.github.com>
2023-02-23 11:47:43 +0300

    Arrays
    day 001: Arrays and their operations

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,23 @@
+
+
+import UIKit
+import Foundation
+
+
+var fruits = ["apple","banana","mango","pineapple"] 
+
+fruit[2] // get the fruit at the 2nd place
+
+team[3] = "avocado" // change 
+
+team.count // count the number of items in the list
+
+team.isEmpty 
+
+team.append("mango") 
+
+
+team.sort() // sort in alphabetical order
+
+team.removeAll() // remove all elements from the array
+
