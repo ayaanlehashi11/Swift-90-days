acf48bd8cec77d819b6f89a30fb67e790a5d0a1a
Ayaanle hashi <91608871+ayaanlehashi11@users.noreply.github.com>
2023-03-23 14:02:01 +0300

    switch statements

---
+++ SwitchStatement.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var dayOfWeek = 4
+
+switch dayOfWeek{
+   case 1:
+     print("saturday")
+   case 2:
+     print("sunday")
+   case 3:
+     print("monday")
+   case 4:
+     print("tuesday")
+   case 5:
+     print("wednesday")
+   case 6:
+     print("thursday")
+   case 7:
+     print("friday")
+}
+
+// output: tuesday
+
